47b368e25d5788e17b828b48735d489bc518b520
MikhailT <tymkaev@gmail.com>
2025-02-28 21:14:06 +1300

    tidy up

--- Frontend/src/app/address/address.ts
+++ Frontend/src/app/address/address.ts
@@ -1,14 +1,6 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {merge} from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatStepperModule} from '@angular/material/stepper';
 
 @Component({
   selector: 'app-address',
@@ -16,35 +8,11 @@ import {MatStepperModule} from '@angular/material/stepper';
   styleUrl: 'address.css',
   standalone: true,
   imports: [
-    MatIconModule,
-    FormsModule, 
-    MatFormFieldModule, 
-    ReactiveFormsModule,
-    MatInputModule, 
     MatSelectModule, 
-    MatCheckboxModule,
-    MatButtonModule,
-    MatStepperModule ],
+    MatButtonModule ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class AddressPage {
-  errorMessage = signal('');
-
-  constructor() {
-    merge()
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
-  }
-
-  updateErrorMessage() {
-    this.errorMessage.set('');
-  }
-
-  hide = signal(true);
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
-  }
 }
 
