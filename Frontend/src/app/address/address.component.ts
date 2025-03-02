f94cd15e6e258c8986ec0ff62b8e860b8e19eeb4
MikhailT <tymkaev@gmail.com>
2025-03-02 22:04:01 +1300

    final touch

--- Frontend/src/app/address/address.component.ts
+++ Frontend/src/app/address/address.component.ts
@@ -1,16 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { MatSelectModule} from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { CountryService } from '../services/country.service';
 import { Country } from '../models/country.model';
 import { CommonModule } from '@angular/common';
 
-import { FormControl, FormGroupDirective, FormGroup, NgForm } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Province } from '../models/province.model';
 import { DataService } from '../services/data.service';
 import { User } from '../models/user.model';
 import { UserDTO } from '../models/user-dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-address',
@@ -20,28 +20,32 @@ import { UserDTO } from '../models/user-dto';
   imports: [
     MatSelectModule, 
     MatButtonModule,
-    CommonModule
+    CommonModule,  
+    ReactiveFormsModule      
    ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 
-export class AddressComponent implements OnInit, ErrorStateMatcher  {
+export class AddressComponent implements OnInit  {
+  formBuilder = inject(FormBuilder);
+  private route = inject(Router)
+  
   user!: User;
   countries!: Country[];
   provinces!: Province[];
   country!: Country;
   province!: Province;
 
+  addressForm = this.formBuilder.group({
+    country: [''],
+    provinces: [''],   
+  });
+
   constructor(
     private dataService: DataService,
-    private countryService: CountryService,){ }
+    private countryService: CountryService){ }
   
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-
   ngOnInit(): void {
       this.dataService.currentUser.subscribe((currentUser)=>
       {
@@ -51,7 +55,16 @@ export class AddressComponent implements OnInit, ErrorStateMatcher  {
         this.countries = result
       });
   }
+  
+  setCountryAndShowProvince(e: any) {
+    this.country = e.value;
+    this.provinces = e.value.provinces;
+  }
 
+  setProvince(e: any) {
+    this.province = e.value;
+  }
+   
   onAddressSubmit() {
     var userDTO ={} as UserDTO
     userDTO.login = this.user.login;
@@ -60,15 +73,5 @@ export class AddressComponent implements OnInit, ErrorStateMatcher  {
 
     this.dataService.SaveUser(userDTO);
   }
-  
-  setProvince(e: any) {
-    this.province = e.value;
-  }
-  
-  setCountryAndShowProvince(e: any) {
-    this.country = e.value;
-    this.provinces = e.value.provinces;
-  }
-  
 }
 
--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,17 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {merge} from 'rxjs';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
@@ -19,87 +11,37 @@ import { User } from '../models/user.model';
   styleUrl: 'login.component.css',
   standalone: true,
   imports: [
-    CommonModule,
-    MatIconModule,
-    FormsModule, 
-    MatFormFieldModule, 
-    ReactiveFormsModule,
-    MatInputModule, 
-    MatSelectModule, 
-    MatCheckboxModule,
-    MatButtonModule
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    MatButtonModule,
+    ReactiveFormsModule
+  ]
 })
 
 export class LoginComponent {
+  formBuilder = inject(FormBuilder);
+  
+  loginForm = this.formBuilder.group({
+    login: ['', [Validators.required, Validators.email]],
+    password: ['', [
+      Validators.required, 
+      Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z]).+$')]
+    ],
+    confirm_password: ['', [
+      Validators.required]
+    ],
+    checkbox: ['', Validators.requiredTrue],
+    
+  });
+
   private dataService  = inject(DataService)
   private route = inject(Router)
-  readonly email = new FormControl('', [Validators.required, Validators.email]);
-  readonly checkbox = new FormControl('', [Validators.requiredTrue]);
-  readonly password = new FormControl('', [Validators.required, Validators.minLength(5)]);
-  readonly confirm_password = new FormControl('', [Validators.required, Validators.minLength(5)]);
-  errorMessage = signal('');
 
-
-  constructor() {
-    merge(this.email.statusChanges, this.email.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateEmailErrorMessage());
-    merge(this.password.statusChanges, this.password.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updatePasswordErrorMessage());
-    merge(this.confirm_password.statusChanges, this.confirm_password.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateConfirmPasswordErrorMessage());
-    merge(this.checkbox.statusChanges, this.checkbox.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateCheckBoxErrorMessage());
-  }
-
-  updateEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
-
-  updateCheckBoxErrorMessage() {
-    if (this.checkbox.hasError('required')) {
-      this.errorMessage.set('You must agree');
-    }
-    else {
-      this.errorMessage.set('');
-    }
-  }
-
-  updatePasswordErrorMessage() {
-    if (this.password.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.password.hasError('password')) {
-      this.errorMessage.set('Not a valid password');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
-
-  updateConfirmPasswordErrorMessage() {
-    if (this.confirm_password.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.confirm_password.hasError('password')) {
-      this.errorMessage.set('Not a valid password');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
+  constructor() { }
 
   navigateToStep2() {
+    console.log(this.loginForm.value);
     const user: User = {
-      login: this.email.value!,
-      password: this.password.value!
+       login: '',//this.loginForm.get("login")!.value!,
+       password: ''//this.password.value!
     };
     this.dataService.changeUserData(user);
     this.route.navigate(['/address']);
